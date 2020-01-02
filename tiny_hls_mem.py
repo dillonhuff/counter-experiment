3aceeec16dc9918d504a0b89cb56da540087f6e0
Dillon Huff <dillonhuff@gmail.com>
2020-01-02 13:05:47 -0800

    Figuring out how to work in reads / writes to ports

--- tiny_hls_mem.py
+++ tiny_hls_mem.py
@@ -53,13 +53,24 @@ class HWProgram:
 
     def __init__(self):
         self.instances = {}
+        self.loops = {}
         self.instructions = []
 
     def add_inst(self, name, mod):
         self.instances[name] = mod
 
-    def add_instruction(self, instr):
-        self.actions.append(instr)
+    def add_instr(self, instr):
+        self.instructions.append(instr)
+
+    def add_loop(self, name, m, e):
+        self.loops[name] = Interval(m, e)
+
+    def sched_expr(self, iis, d):
+        li = LinearExpr()
+        for ci in iis:
+            li.coeffs[ci[1]] = ci[0]
+        li.d = d
+        return li
 
 def outpt(name):
     return (name, True, 1)
@@ -71,7 +82,28 @@ p = HWProgram();
 world = Module("_world_", [outpt("clk"), outpt("rst"), outpt("valid"), inpt("res"), outpt("in")], "")
 p.add_inst("world", world)
 
-reg = Module("reg_1", [inpt("clk"), inpt("rst"), inpt("d"), outpt("q")])
+reg = Module("reg_1", [inpt("clk"), inpt("rst"), inpt("d"), outpt("q")], "")
 p.add_inst("data", reg)
+
+p.add_loop("x", 0, 10)
+
+# Create a time for the operation, then create a swvalue from the port value we are reading
+# and the operation time, then create an instruction that uses that and the reg?
+wire_read_time = p.sched_expr([(1, "x")], 0)
+in_wire = HWVal("world", "in")
+in_val = SWVal("wire_val", in_wire, wire_read_time)
+
+write_reg = HWInstr("write", "data", [in_wire])
+
+p.add_instr(write_reg)
+
+out_set_time = p.sched_expr([(1, "x")], 1)
+out_wire = HWVal("world", "res")
+read_reg = HWInstr("read", "data", [])
+
+write_out = HWInstr("write_pt_res", "world", [read_reg])
+
+print('Done.')
+
 
 
