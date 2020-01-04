2cd049eb9b9ec0e5441cc8a0122e93f488a538f8
Dillon Huff <dillonhuff@gmail.com>
2020-01-04 13:11:25 -0800

    Not sure how to count conditions since unit

--- builtins.v
+++ builtins.v
@@ -30,11 +30,33 @@ module n_clks_since_signal(input clk, input rst, input signal, output out);
   wire no_signal_yet;
 
   clks_since_signal sig_cntr(.clk(clk), .rst(rst), .signal(signal), .no_signal_yet(no_signal_yet), .num(num_clks));
-  //.clk(clk), .rst(rst), .signal(signal), .num(num_clks), .no_signal_yet(.no_signal_yet));
 
   assign out = !no_signal_yet & (num_clks == N);
 
 endmodule
 
+module condition_at_last_signal(input clk, input rst, input signal, input condition, output out, output no_signal_yet);
+
+  reg signal_seen;
+  reg condition_value_at_last_signal;
+  assign no_signal_yet = signal ? 0 : signal_seen;
+  assign out = signal ? condition : condition_value_at_last_signal;
+
+  always @(posedge clk) begin
+    if (rst) begin
+      signal_seen <= 0;
+    end else begin
+      if (signal) begin
+        signal_seen <= 1;
+        condition_value_at_last_signal <= condition;
+      end else begin
+      end
+    end
+  end
+  
+endmodule
+
+// Self transition is: II clks since last signal and
+// condition_at_last_signal(happening, !x_at_trip_count)
 module reg_1(input clk, input rst, input en, input d, output q);
 endmodule
